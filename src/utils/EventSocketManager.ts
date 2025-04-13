32142c36622989a749d935fae1c20d615ba8425e
Mohamad-Hag <mohamadhag99@gmail.com>
2025-04-13 18:53:52 +0400

    [UPDATE] Add Reaction Feature

--- src/utils/EventSocketManager.ts
+++ src/utils/EventSocketManager.ts
@@ -4,7 +4,12 @@ import SocketManager from "./SocketManager";
 class EventSocketManager {
   static initialize(socket: Socket) {
     // You can perform any initialization logic for the connectedsocket here
-    socket.emit("message", "Hello World");
+    socket.on("wave", (data) => {
+      console.log(
+        `Joiner with Id ${data.guest.id} waved to event ${data.eventId}`
+      );
+      SocketManager.emitToRoom(`event-${data.eventId}`, "joinerWaved", data);
+    });
   }
 }
 
